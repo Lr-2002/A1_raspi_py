65f4a3456a23c6cb820518174200ecdab5fae0b5
Lr-2002 <2629651228@qq.com>
2023-08-09 12:12:04 +0800

    write setup.py

---
+++ py/setup.py
@@ -0,0 +1,12 @@
+from setuptools import  setup, find_packages
+
+setup(
+    name="a1_py",
+    version="1.0",
+    description="install to site-packages",
+    author="Lr-2002",
+    author_email="2629651228@qq.com",
+    url="https://github.com/Lr-2002/A1_raspi_py",
+    packages=find_packages(),
+
+)
